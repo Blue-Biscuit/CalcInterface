c782acdade0343951783d248798f7a94cb6cc789
Blue-Biscuit <ahuffman9000@gmail.com>
2021-02-13 18:48:10 -0500

    Class framework.

---
+++ src/Numeric.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//	LIB INCLUDES
+
+#include "Solution/Solution.h"
+#include "Solution/Solvable.h"
+
+namespace cc
+{
+	//	Abstract class for a numeric expression, allowing for
+	//	polymorphic numbers.
+	class Numeric : Solvable
+	{
+
+	};
+}
